e11bbae7bd1d49cbb49349629c7ddc5961919020
Martin L. Arias Paz <martin.arias@fizzmod.com>
2022-04-09 12:11:26 -0300

    interfaces updated

--- src/02-components-patterns/interfaces/interfaces.ts
+++ src/02-components-patterns/interfaces/interfaces.ts
@@ -24,7 +24,7 @@ export interface ButtonProps {
 
 export interface ProductCardHOCProps {
   ({ children, product }: ProductCardProps): JSX.Element;
-  Title: ({ title }: { title?: string | undefined }) => JSX.Element;
-  Image: ({ img }: { img?: string | undefined }) => JSX.Element;
+  Title: ({ title }: { title?: string  }) => JSX.Element;
+  Image: ({ img }: { img?: string  }) => JSX.Element;
   Buttons: () => JSX.Element;
 }
